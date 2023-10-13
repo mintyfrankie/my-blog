0b7722098bdce2c3c7e881a3aa8b30f98cb72fe0
yunan.wang <77310871+mintyfrankie@users.noreply.github.com>
2023-10-13 14:13:51 +0200

    feat: set global color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,25 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
+    colors: {
+      accent: {
+        50: '#f3f8fa',
+        100: '#e9f1f6',
+        200: '#d8e4ed',
+        300: '#bfd2e2',
+        400: '#a5bad4',
+        500: '#96aaca',
+        600: '#7689b5',
+        700: '#64749e',
+        800: '#536080',
+        900: '#475268',
+        950: '#2a303c',
+      },
+    },
   },
   plugins: [],
 }
