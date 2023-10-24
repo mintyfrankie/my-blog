4350e3a53ee41978cbf645a6db91620f1579fd2a
yunan.wang <77310871+mintyfrankie@users.noreply.github.com>
2023-10-24 19:25:25 +0200

    feat: add i18n utilities

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+  en: 'English',
+  fr: 'Français',
+  zh: '中文',
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  },
+  fr: {
+    'nav.home': 'Accueil',
+    'nav.about': 'À propos',
+  },
+  zh: {
+    'nav.home': '首页',
+    'nav.about': '关于',
+  },
+} as const
