d909f3e2f53edee4934b341dd93a8f590a07146c
yunan.wang <77310871+mintyfrankie@users.noreply.github.com>
2023-10-25 08:02:33 +0200

    feat: i18n NavBar

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,3 +1,5 @@
+import { config } from 'site.config'
+
 export const languages = {
   en: 'English',
   fr: 'Français',
@@ -8,12 +10,15 @@ export const defaultLang = 'en'
 
 export const ui = {
   en: {
+    title: config.siteTitle,
     TranslatedBanner: 'Experimental: this article is translated by AI.',
   },
   fr: {
+    title: config.siteTitle,
     TranslatedBanner: "Expérimental : cet article est traduit par l'IA.",
   },
   zh: {
+    title: config.siteTitleZh,
     TranslatedBanner: '测试：本文经由 AI 翻译。',
   },
 } as const
